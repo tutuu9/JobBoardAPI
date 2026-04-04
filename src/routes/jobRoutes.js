ff54b66815aef0b7a5b634730d2aa397797ab099
TUTUU9 <tarasuzun@gmail.com>
2026-04-04 11:11:21 +0200

    fix: router in jobRoutes (POST -> GET) getAllJobs

--- src/routes/jobRoutes.js
+++ src/routes/jobRoutes.js
@@ -5,7 +5,7 @@ const authMiddleware = require('../middlewares/authMiddleware');
 const { createJob, getAllJobs, updateJob, deleteJob } = require('../controllers/jobController');
 
 router.post('/', authMiddleware, createJob);
-router.post('/', getAllJobs);
+router.get('/', getAllJobs);
 router.put('/:id', authMiddleware, updateJob);
 router.delete('/:id', authMiddleware, deleteJob);
 module.exports = router;
